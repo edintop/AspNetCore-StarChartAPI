0511b5fe3f3cbd54edfeada93037efa4554c2f2c
edintop <82210722+edintop@users.noreply.github.com>
2021-04-15 21:11:06 +0200

    Fix wrong method name

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -96,7 +96,7 @@ namespace StarChart.Controllers
         }
 
         [HttpDelete("{id}")]
-        public IActionResult Remove(int id, string name)
+        public IActionResult Delete(int id, string name)
         {
             var cels = _context.CelestialObjects.Where(n => n.Id == id || n.OrbitedObjectId == id);
             if (!cels.Any()) return NotFound();
